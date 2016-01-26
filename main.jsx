f871be054f25a9b26339a731f2545acaadbdd54b
Jacob Kopczynski <ja.kopczynski@gmail.com>
2016-01-26 10:43:25 -0800

    testing requirements for react-shuffle

--- main.jsx
+++ main.jsx
@@ -1,6 +1,8 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var ReactCSSTransitionGroup = require('react-addons-css-transition-group');
+var Shuffle = require('react-shuffle');
+
 
 var start = new Date().getTime();
 
@@ -25,18 +27,28 @@ return (
 });
 
 var ListComponent = React.createClass({
+  shuffleChildren() {
+    this.setState({
+      children: shuffle(this.state.children)
+    });
+  },
   render: function() {
-    return <ReactCSSTransitionGroup transitionName="list"
-          transitionEnterTimeout={5000} transitionLeaveTimeout={3000}>
-        {this.props.children}
-    </ReactCSSTransitionGroup>
+    return <div>
+    {/*<button type="button" onClick={this.shuffleChildren}>Shuffle Children</button>*/}
+      <ReactCSSTransitionGroup transitionName="list" 
+        transitionEnterTimeout={5000} transitionLeaveTimeout={3000}>
+        <Shuffle duration={500} fade={false}>
+          {this.props.children}
+        </Shuffle>
+      </ReactCSSTransitionGroup>
+    </div>
   }
 });
 var ListComponentFactory = React.createFactory(ListComponent);
 
 function shuffle(o){
   for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = 
-  o[--i], o[i] = o[j], o[j] = x, x['key'] = i*j);
+  o[--i], o[i] = o[j], o[j] = x);
   return o;
 }
 
@@ -49,6 +61,55 @@ function someData() {
       data: ((new Date().getTime() - start)/1000).toFixed()}]);
 }
 
+const alphabet = [
+  'a','b','c','d','e','f','g','h','i','j','k','l','m',
+  'n','o','p','q','r','s','t','u','v','w','x','y','z']
+
+const App = React.createClass({
+  getInitialState() {
+    return {
+      children: alphabet,
+      filtered: false
+    }
+  },
+  shuffleChildren() {
+    this.setState({
+      children: shuffle(this.state.children)
+    });
+  },
+  filterChildren() {
+    if (this.state.filtered === false) {
+      var newChildren = this.state.children.filter(function(child,index){
+        if (index % 2 ===0) {
+          return child
+        }
+      });
+      this.setState({
+        children: newChildren,
+        filtered: true
+      });
+    } else {
+      this.setState({
+        children: alphabet,
+        filtered: false
+      });
+    }
+  },
+  render() {
+    return (
+      <div className="demo">
+      {/*<button type="button" onClick={this.shuffleChildren}>Shuffle Children</button>
+        <button type="button" onClick={this.filterChildren}>Filter Children</button>*/}
+        <Shuffle duration={500} fade={false}>
+          {this.props.children}
+        </Shuffle>
+      </div>
+    )
+  }
+});
+
+const content = document.getElementById('content');
+
 setInterval(function() {
   ReactDOM.render(
     <ListComponent>
@@ -58,4 +119,13 @@ setInterval(function() {
     </ListComponent>,
     document.getElementById('list')
   );
+  ReactDOM.render(<App>
+    {shuffle(alphabet).map(function(letter) {
+        return (
+              <div className="tile" key={letter}>
+                <img src={"http://placehold.it/100x100&text=" + letter} />
+              </div>
+            )
+      })}
+    </App>, document.getElementById('content'))
 }, 2000);
